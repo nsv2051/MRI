9031d8afb12b184b194e3c2296434bbe7164c1c0
NANSIYV <shuai2051@gmail.com>
2022-02-08 11:35:11 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,18 +5,18 @@ new Env('下载配置文件模板到config文件夹');
 COMMENT
 
 echo "1.自动更新task_before.sh模板"
-curl -L https://cdn.jsdelivr.net/gh/shuai2051/MRI@stable/task_before.sh -o /ql/sample/task_before.s.sh && cp -rf /ql/sample/task_before.s.sh /ql/config
+curl -L https://cdn.jsdelivr.net/gh/nsv2051/MRI@stable/task_before.sh -o /ql/sample/task_before.s.sh && cp -rf /ql/sample/task_before.s.sh /ql/config
 echo "2.自动更新config.sh模板"
-curl -L https://cdn.jsdelivr.net/gh/shuai2051/MRI@stable/config_sample.sh -o /ql/sample/config.s.sh && cp -rf /ql/sample/config.s.sh /ql/config
+curl -L https://cdn.jsdelivr.net/gh/nsv2051/MRI@stable/config_sample.sh -o /ql/sample/config.s.sh && cp -rf /ql/sample/config.s.sh /ql/config
 echo "3.自动更新code.sh模板"
-curl -L https://cdn.jsdelivr.net/gh/shuai2051/MRI@stable/code.sh -o /ql/sample/code.s.sh && cp -rf /ql/sample/code.s.sh /ql/config
+curl -L https://cdn.jsdelivr.net/gh/nsv2051/MRI@stable/code.sh -o /ql/sample/code.s.sh && cp -rf /ql/sample/code.s.sh /ql/config
 echo "4.自动更新extra.sh模板"
-curl -L https://cdn.jsdelivr.net/gh/shuai2051/MRI@stable/extra.sh -o /ql/sample/extra.s.sh && cp -rf /ql/sample/extra.s.sh /ql/config
+curl -L https://cdn.jsdelivr.net/gh/nsv2051/MRI@stable/extra.sh -o /ql/sample/extra.s.sh && cp -rf /ql/sample/extra.s.sh /ql/config
 echo "5.自动更新extra2.sh模板"
-# curl -L https://cdn.jsdelivr.net/gh/shuai2051/MRI@stable/extra2.sh -o /ql/scripts/extra2.sh
-ql raw https://raw.githubusercontent.com/shuai2051/MRI/stable/extra2.sh
+# curl -L https://cdn.jsdelivr.net/gh/nsv2051/MRI@stable/extra2.sh -o /ql/scripts/extra2.sh
+ql raw https://raw.githubusercontent.com/nsv2051/MRI/stable/extra2.sh
 echo "6.自动下载ckck2.sh文件"
-# curl -L https://cdn.jsdelivr.net/gh/shuai2051/MRI@stable/ckck2.sh -o /ql/scripts/ckck2.sh
-ql raw https://raw.githubusercontent.com/shuai2051/MRI/stable/ckck2.sh
+# curl -L https://cdn.jsdelivr.net/gh/nsv2051/MRI@stable/ckck2.sh -o /ql/scripts/ckck2.sh
+ql raw https://raw.githubusercontent.com/nsv2051/MRI/stable/ckck2.sh
 echo "配置完毕"
 
