02ad9205d260cb331858b32ee71cdc12812ae7fa
NANSIYV <shuai2051@gmail.com>
2022-02-14 16:21:04 +0800

    Add files via upload

--- DIY-dep.sh
+++ DIY-dep.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # new Env('复制配置文件');
 echo "开始下载"
-wget https://ghproxy.com/https://raw.githubusercontent.com/ccwav/QLScript2/main/ql.js -o ql-log -O /ql/deps/ql.js
-wget https://ghproxy.com/https://raw.githubusercontent.com/ccwav/QLScript2/main/sendNotify.js -o sendNotify-log -O /ql/deps/sendNotify.js
-wget https://ghproxy.com/https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/JS_USER_AGENTS.js -o JS_USER_AGENTS-log -O /ql/deps/JS_USER_AGENTS.js
-wget https://ghproxy.com/https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/USER_AGENTS.js -o USER_AGENTS-log -O /ql/deps/USER_AGENTS.js
-wget https://ghproxy.com/https://raw.githubusercontent.com/Oreomeow/VIP/main/Scripts/sh/Helpcode2.8/jdCookie.js -o jdCookie-log -O /ql/deps/jdCookie.js
+wget https://ghproxy.com/https://raw.githubusercontent.com/ccwav/QLScript2/main/ql.js -o ql.log -O /ql/deps/ql.js
+wget https://ghproxy.com/https://raw.githubusercontent.com/ccwav/QLScript2/main/sendNotify.js -o sendNotify.log -O /ql/deps/sendNotify.js
+wget https://ghproxy.com/https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/JS_USER_AGENTS.js -o JS_USER_AGENTS.log -O /ql/deps/JS_USER_AGENTS.js
+wget https://ghproxy.com/https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/USER_AGENTS.js -o USER_AGENTS.log -O /ql/deps/USER_AGENTS.js
+wget https://ghproxy.com/https://raw.githubusercontent.com/Oreomeow/VIP/main/Scripts/sh/Helpcode2.8/jdCookie.js -o jdCookie.log -O /ql/deps/jdCookie.js
 # wget https://ghproxy.com/https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jdCookie.js -o jdCookie-log -O /ql/deps/jdCookie.js
 echo "开始复制到各目录"
 cp /ql/deps/ql.js /ql/scripts/ql.js
