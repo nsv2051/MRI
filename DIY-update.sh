a626b222dea656d33f972e30d85af4feb21f36cc
NANSIYV <shuai2051@gmail.com>
2022-02-15 14:53:36 +0800

    Update DIY-update.sh

--- DIY-update.sh
+++ DIY-update.sh
@@ -12,7 +12,7 @@ echo "2.自动更新config.sh模板"
 wget https://git.shuai2051.workers.dev/https://raw.githubusercontent.com/nsv2051/MRI/stable/config_sample.sh -o config_sample.log -O /ql/sample/config.s.sh && cp -rf /ql/sample/config.s.sh /ql/config
 echo "3.自动更新code.sh模板"
 # curl -L https://cdn.jsdelivr.net/gh/shuai2051/MRI@stable/code.sh -o /ql/sample/code.s.sh && cp -rf /ql/sample/code.s.sh /ql/config
-wget https://git.shuai2051.workers.dev/https://raw.githubusercontent.com/nsv2051/MRI/stable/code.sh -o code.log-O /ql/sample/code.s.sh && cp -rf /ql/sample/code.s.sh /ql/config
+wget https://git.shuai2051.workers.dev/https://raw.githubusercontent.com/nsv2051/MRI/stable/code.sh -o code.log -O /ql/sample/code.s.sh && cp -rf /ql/sample/code.s.sh /ql/config
 echo "4.自动更新extra.sh模板"
 # curl -L https://cdn.jsdelivr.net/gh/shuai2051/MRI@stable/extra.sh -o /ql/sample/extra.s.sh && cp -rf /ql/sample/extra.s.sh /ql/config
 wget https://git.shuai2051.workers.dev/https://raw.githubusercontent.com/nsv2051/MRI/stable/extra.sh -o extra.log -O /ql/sample/extra.s.sh && cp -rf /ql/sample/extra.s.sh /ql/config
